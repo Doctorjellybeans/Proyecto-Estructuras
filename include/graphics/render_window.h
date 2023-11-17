f7f00df4f9de64891f8019014535cc58d03cd662
MatiasPUCV <142541831+MatiasPUCV@users.noreply.github.com>
2023-11-17 19:11:55 -0300

    Comentarios en render_window.h

--- include/graphics/render_window.h
+++ include/graphics/render_window.h
@@ -20,14 +20,16 @@ public:
     /* Revisa si la ventana esta abierta */
     bool is_open() { return !this->is_closed; };
 
+    /* Dibuja un objeto */
+    void draw(const Drawable& drawable) const;
+
     /* Muestra un fotograma */
     void display();
 
     /* Limpia un fotograma */
     void clear();
 
-    /* Dibuja un objeto */
-    void draw(const Drawable& drawable) const;
+    /* Carga una textura */
     Texture* load_texture(const char* filename);
     SDL_Renderer* get_renderer() const { return this->renderer; }
 
