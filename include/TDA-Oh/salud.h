769690567116379e8f17d147ae64defc16420bc6
Doctorjellybeans <virozalesm@gmail.com>
2023-11-24 21:55:06 -0300

    Adicion de codigo en salud.h

--- include/TDA-Oh/salud.h
+++ include/TDA-Oh/salud.h
@@ -20,46 +20,128 @@ class Salud {
 
     ~Salud() {}
 
-    void cambiarTipoTDA(TipoTDA nuevoTipo) {
-        tipoActual = nuevoTipo;
+    void obtenerTipoActual() const {
+        return tipoActual;
     }
 
-    bool esValido(Carta carta) {
+    int esValido(Carta carta) {
         TipoOperacion operacion = carta.obtenerOperacion();
         TipoCarta tipoCarta = carta.obtenerTipo();
 
-        switch (tipoCarta) {
-            case TipoCarta::VIDA:
-                if (operacion == TipoOperacion::Apilar) {
-                    return true;
-                } else if {
-                    
-                }
-            case TipoCarta::DANIO:
-                // algo
-            case TipoCarta::EFECTO
-                // algo
-            default
-                return false;
+        if (tipoActual == TipoTDA::PILA) {
+            
+            switch (TipoCarta) {
+                case TipoCarta::DANIO:
+                    if (operacion == TipoOperacion::Desapilar) {
+                        return 1;
+                    }
+                    return -1;
+
+                case TipoCarta::VIDA:
+                    if (operacion == TipoOperacion::Apilar) {
+                        return 2;
+                    }
+                    return -1;
+
+                default:
+                    break;
+            }
+
+        } else if (tipoActual == TipoTDA::COLA) {
+            
+            switch (TipoCarta) {
+                case TipoCarta::DANIO:
+                    if (operacion == TipoOperacion::Desencolar) {
+                        return 1;
+                    }
+                    return -1;
+
+                case TipoCarta::VIDA:
+                    if (operacion == TipoOperacion::Encolar) {
+                        return 2;
+                    }
+                    return -1;
+
+                default:
+                    break;
+            }
+
+        } else if (tipoActual == TipoTDA::LISTA) {
+            switch (TipoCarta) {
+                case TipoCarta::DANIO:
+                    if (operacion == TipoOperacion::EliminarActual) {
+                        return 1;
+                    } 
+                    return -1;
+
+                case TipoCarta::VIDA:
+                    if (operacion == TipoOperacion::InsertarActual) {
+                        return 2;
+                    }
+                    return -1;
+
+                default:
+                    break;
+            }
+        } else if (tipoCarta == TipoCarta::EFECTO) {
+            return 3
         }
     }
 
-    bool esValido(Carta carta) {
-
+    void causarDanio(int puntajeCarta) {
+        for (int i = 0; i < puntajeCarta; i++) {
+            pilaSalud.desapilar();
+            colaSalud.pop();
+            listaSalud.eliminarAtras();
+        }
     }
+
+    void curarVida(int puntajeCarta) {
+        for (int i = 0; i < puntajeCarta; i++) {
+            pilaSalud.desapilar();
+            colaSalud.pop();
+            listaSalud.eliminarAtras();
+        }
+    }
+
+    void cambiarSiguiente() {
+        switch (tipoActual) {
+            case TipoTDA::Pila:
+                tipoActual = TipoTDA::Cola;
+                break;
+            case TipoTDA::Cola:
+                tipoActual = TipoTDA::Lista;
+                break;
+            case TipoTDA::Lista:
+                tipoActual = TipoTDA::Pila;
+                break;
+        }
+    }
+
 
     void operacion(Carta carta) {
-        switch (tipoActual) {
-            case TipoTDA::PILA:
+        int puntajeCarta = carta.obtenerPuntaje();
+        int tipoCarta = esValido(carta);
 
-        }
-        {
-        case /* constant-expression */:
-            /* code */
-            break;
-        
-        default:
-            break;
+        if (tipoCarta == -1) { // No se puede operar
+            
+        } else { // Se puede operar
+            
+            switch (tipoCarta) {
+                case 1: // DANIO
+                    causarDanio();
+                    break;
+                case 2: // SALUD
+                    curarVida();
+                    break;
+
+                case 3: // EFECTO
+                    cambiarSiguiente();
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 
