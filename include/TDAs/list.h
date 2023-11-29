527d4daff8998ff1dcbb981a8a2a6d7ef61096ac
MatiasPUCV <142541831+MatiasPUCV@users.noreply.github.com>
2023-11-29 10:19:44 -0300

    Mapahash

---
+++ include/TDAs/list.h
@@ -0,0 +1,263 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <iostream>
+
+template <typename T>
+class List
+{
+public:
+
+    List();
+    ~List() = default;
+
+    T* first();
+    T* last();
+
+    T* next();
+    T* prev(); 
+    T* current();
+
+    T* search(T data);
+
+    void pushFront(T* data);
+    void pushBack(T* data);
+    void pushCurrent(T* data);
+
+    T* popFront();
+    T* popBack();
+    T* popCurrent();
+
+    bool empty() { return this->_head == nullptr; }
+
+private:
+
+    template <typename K>
+    struct Node
+    {
+        Node(K value) : _data(value), _next(nullptr), _prev(nullptr) {}
+
+        K _data;
+        Node<K>* _next;
+        Node<K>* _prev;
+    };
+
+    Node<T*>* _current;
+    Node<T*>* _head;
+    Node<T*>* _tail;
+};
+
+template <typename T>
+List<T>::List()
+    : _current(nullptr), _head(nullptr), _tail(nullptr)
+{
+
+}
+
+template <typename T>
+T* List<T>::first()
+{
+    if (this->_head == nullptr)
+        return nullptr;
+
+    this->_current = this->_head;
+    return this->_current->_data;
+}
+
+template <typename T>
+T* List<T>::last()
+{
+    if (this->_tail == nullptr)
+        return nullptr;
+
+    this->_current = this->_tail;
+    return this->_current->_data;
+}
+
+template <typename T>
+T* List<T>::next()
+{
+    if (this->_current == nullptr)
+        return nullptr;
+
+    this->_current = this->_current->_next;
+    if (this->_current == nullptr)
+      return nullptr;
+
+    return this->_current->_data;
+}
+
+template <typename T>
+T* List<T>::prev()
+{
+    if (this->_current == nullptr)
+        return nullptr;
+
+    this->_current = this->_current->_tail;
+    if (this->_current == nullptr)
+      return nullptr;
+
+    return this->_current->_data;
+}
+
+template <typename T>
+T* List<T>::current()
+{
+    if (this->_current == nullptr)
+        return nullptr;
+
+    return this->_current->data;
+}
+
+template <typename T>
+void List<T>::pushFront(T* data)
+{
+    Node<T*>* node = new Node<T*>(data);
+
+    if (empty())
+    {
+        this->_head = node;
+        this->_tail = node;
+
+        return;
+    }
+
+    this->_head->_prev = node;
+    node->_next = this->_head;
+
+    this->_head = node;
+}
+
+template <typename T>
+void List<T>::pushBack(T* data)
+{
+    Node<T*>* node = new Node<T*>(data);
+
+    if (empty())
+    {
+        this->_head = node;
+        this->_tail = node;
+
+        return;
+    }
+
+    this->_tail->_next = node;
+    node->_prev = this->_tail;
+
+    this->_tail = node;
+}
+
+template <typename T>
+void List<T>::pushCurrent(T* data)
+{
+    if (data == nullptr || this->_current == nullptr)
+      return;
+
+    Node<T*>* node = new Node<T*>(data);
+
+    if (empty())
+    {
+        this->_head = node;
+        this->_tail = node;
+
+        return;
+    }
+
+    node->_prev = this->_current;
+
+    if (this->_current->_next != nullptr)
+    {
+        node->_next = this->_current->_next;
+        this->_current->_next->_prev = node;
+    }
+    else
+    {
+        this->_tail = node;
+    }
+
+    this->_current->_next = node;
+}
+
+template <typename T>
+T* List<T>::popFront()
+{
+    if (this->_head == nullptr)
+        return nullptr;
+
+    Node<T*>* node = this->_head;
+
+    this->_head = this->_head->_next;
+
+    if (this->_head != nullptr)
+        this->_head->_prev = nullptr;
+
+    this->_current = this->_head;
+
+    T* data = node->_data;
+    delete node;
+
+    return data;
+}
+
+template <typename T>
+T* List<T>::popBack()
+{
+    if (this->_head == nullptr)
+        return nullptr;
+
+    Node<T*>* node = this->_tail;
+
+    this->_tail = this->_tail->_prev;
+
+    if (this->_tail != nullptr)
+        this->_tail->_next = nullptr;
+
+    this->_current = this->_tail;
+
+    T* data = node->_data;
+    delete node;
+
+    return data;
+}
+
+template <typename T>
+T* List<T>::popCurrent()
+{
+    if (this->_current == nullptr)
+        return nullptr;
+
+    if (this->_current->_next == nullptr)
+        return popBack();
+
+    if (this->_current->_prev == nullptr)
+        return popFront();
+
+    Node<T*>* node = this->_current;
+  
+    node->_prev->_next = node->_next;
+    node->_next->_prev = node->_prev;
+
+    this->_current = node->_next;
+
+    T* data = node->_data;
+    delete node;
+
+    return data;
+}
+
+template <typename T>
+T* List<T>::search(T data)
+{
+    T* ptr = first();
+    while (ptr != nullptr)
+    {
+        if (*ptr == data)
+            return ptr;
+
+        ptr = next();
+    }
+    
+    return nullptr;
+}
+
+#endif //LIST_H
---
+++ include/TDAs/map.h
@@ -0,0 +1,210 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include "list.h"
+#include <ctype.h>
+#include <iostream>
+
+template <typename T1, typename T2>
+struct Pair
+{
+    Pair(T1 key, T2 value) : key(key), value(value) {}
+    ~Pair() = default;
+
+    T1 key;
+    T2 value;
+};
+
+// Le añade un signifacado al operador '==' para Pair
+template <typename T1, typename T2>
+inline bool operator==(const Pair<T1, T2>& left, const Pair<T1, T2>& right)
+{
+    return (left.key == right.key) && (left.value == right.value);
+}
+
+// Mapa no-ordenado
+template <typename T1, typename T2>
+class Map
+{
+public:
+
+    Map();
+    ~Map();
+
+    using _Pair = Pair<T1, T2*>;
+
+    void insert(T1 key, T2* value);
+    void erase(T1 key);
+
+    T2* search(T1 key);
+    T2* first();
+    T2* next();
+
+    size_t hash(T1 key)
+    {
+        size_t hash = 0;
+        unsigned char* ptr;
+
+        for (ptr = (unsigned char*)(&key); *ptr != '\0'; ptr++)
+            hash += hash * 32 + tolower(*ptr);
+
+        return hash % this->capacity;
+    }
+
+private:
+
+    void enlarge();
+    List<_Pair>* buckets;
+
+
+    size_t size;
+    size_t capacity;
+    size_t current;
+
+};
+
+template <typename T1, typename T2>
+Map<T1, T2>::Map()
+    : size(0), capacity(2), current(-1)
+{
+    this->buckets = new List<_Pair>[this->capacity];
+}
+
+template <typename T1, typename T2>
+Map<T1, T2>::~Map()
+{
+    delete[] this->buckets;
+}
+
+
+template <typename T1, typename T2>
+void Map<T1, T2>::insert(T1 key, T2* value)
+{
+    size_t pos = hash(key);
+    List<_Pair>& list = this->buckets[pos];
+
+    _Pair* ptr = list.first();
+    while (ptr != nullptr)
+    {
+        if (ptr->key == key)
+        {
+            ptr->value = value;
+            return;
+        }
+
+        ptr = list.next();
+    }
+
+    // Nuevo 'Pair'
+    _Pair* pair = new _Pair(key, value);
+    list.pushBack(pair);
+
+    this->size++;
+
+    // Agranda el mapa, si es nesecesario
+    float cap70 = this->capacity * 0.7;
+    if (this->size >= cap70)
+        enlarge();
+
+    return;
+}
+
+template <typename T1, typename T2>
+void Map<T1, T2>::erase(T1 key)
+{
+    size_t pos = hash(key);
+    List<_Pair>& list = this->buckets[pos];
+
+    _Pair* ptr = list.first();
+    while (ptr != nullptr)
+    {
+        if (ptr->key == key)
+        {
+            list.popCurrent();
+            delete ptr;
+
+            this->size--;
+            return;
+        }
+
+        ptr = list.next();
+    }
+}
+
+template <typename T1, typename T2>
+T2* Map<T1, T2>::search(T1 key)
+{
+    size_t pos = hash(key);
+    List<_Pair>& list = this->buckets[pos];
+
+    _Pair* ptr = list.first();
+    while (ptr != nullptr)
+    {
+        if (ptr->key == key)
+            return ptr->value;
+
+        ptr = list.next();
+    }
+
+    return nullptr;
+}
+
+template <typename T1, typename T2>
+T2* Map<T1, T2>::first()
+{
+    this->current = 0;
+    while (this->current < this->capacity && buckets[current].empty())
+        current++;
+
+    if (this->current < this->capacity)
+        return buckets[current].first()->value;
+
+    return nullptr;
+}
+
+
+template <typename T1, typename T2>
+T2* Map<T1, T2>::next()
+{
+    if (this->current == this->capacity - 1)
+        return nullptr;
+
+    current++;
+    while (this->current < this->capacity && buckets[current].empty())
+        current++;
+
+    if (this->current < this->capacity)
+        return buckets[current].first()->value;
+
+    return nullptr;
+}
+
+template <typename T1, typename T2>
+void Map<T1, T2>::enlarge()
+{
+    List<_Pair>* oldBuckets = this->buckets;
+    size_t oldCapacity = this->capacity;
+
+    this->size = 0;
+    this->capacity *= 2;
+
+    this->buckets = new List<_Pair>[this->capacity];
+
+    for (size_t i = 0; i < oldCapacity; i++)
+    {
+        List<_Pair> list = this->buckets[i];
+        _Pair* ptr = list.first();
+        while (ptr != nullptr)
+        {
+            insert(ptr->key, ptr->value);
+            delete ptr;
+
+            ptr = list.next();
+        }
+        
+    }
+
+    delete[] oldBuckets;
+}
+
+#endif //MAP_H
