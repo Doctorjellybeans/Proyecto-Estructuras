79494b1f11e032b055fcb6293f62441b1449cdb0
Doctorjellybeans <virozalesm@gmail.com>
2023-12-01 17:49:50 -0300

    adicion de enum.h

---
+++ include/TDA-Oh/enum.h
@@ -0,0 +1,16 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+enum class TDAType { STACK, QUEUE, LIST, SPECIAL };
+
+enum class TDACardOperation {
+    PUSH,
+    POP,
+    ENQUEUE,
+    DEQUEUE,
+    INSERT,
+    DELETE,
+    CHANGETDA
+};
+
+#endif // ENUM_H
